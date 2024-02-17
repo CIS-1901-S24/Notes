57c31bb0a48c6dcf311909887de4f3835d2d6dc2
alnasir7 <alnasirvx@gmail.com>
2024-02-17 00:17:32 -0500

    class 6

---
+++ 06/concepts.cpp
@@ -0,0 +1,30 @@
+#include <concepts>
+#include <iostream>
+
+using namespace std;
+
+class foo{};
+
+template<typename T>
+concept addable = requires (const T & x) { x + x; }; // requires x + x to be valid
+
+template <typename T>
+int f (T t)
+requires std::totally_ordered<T>
+{
+    return 0;
+}
+
+template<addable T> // could also use requires
+T add(const T & a, const T & b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << add(1, 2);
+
+    // Uncomment to see the error
+    // cout << add(foo(), foo());
+}
